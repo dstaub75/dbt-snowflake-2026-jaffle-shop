7e95c1b6615ac1a96eb30c15eb2a9a4a307369c9
David Staub <dstaub75@gmail.com>
2026-01-27 22:33:33 +0000

    fix

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -2,7 +2,7 @@ select
     id as payment_id,
     orderid as order_id,
     paymentmethod as payment_method,
-    status,
+    status as order_status,
     -- amount is stored in cents, convert it to dollars
     amount / 100 as amount,
     created as created_at
