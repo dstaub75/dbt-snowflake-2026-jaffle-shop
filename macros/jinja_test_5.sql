300323a40ca4a9e5f3ba34af890c9f35f539d04b
David Staub <dstaub75@gmail.com>
2026-02-17 15:32:45 +0000

    add orders_pivoted sql

--- macros/jinja_test_5.sql
+++ macros/jinja_test_5.sql
@@ -9,8 +9,11 @@ with payments as
     select order_id,
         
         {% set payment_methods = ['bank_transfer','coupon','credit_card','gift_card'] %}
-        {% for payment_method in payment_methods %}
-            sum(case when payment_method = '{{payment_method}}' then amount else 0 end) as {{payment_method}}_amount,
+        {% for method in payment_methods %}
+            sum(case when payment_method = '{{method}}' then amount else 0 end) as {{payment_method}}_amount
+            {% if not loop.last %}
+                ,
+            {% endif %}
         {% endfor %}
     from payments
     group by 1
